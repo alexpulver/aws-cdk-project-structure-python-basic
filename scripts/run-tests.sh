e01bca7376639b3caa6c81f7d2a41051bf7b5109
Alex Pulver <alex.pulver@gmail.com>
2021-08-10 09:32:08 +0300

    Add comments to run-tests.sh

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,16 +5,36 @@ set -o verbose
 
 targets=(api app.py constants.py deployment.py)
 
+# Find common security issues (https://bandit.readthedocs.io)
 bandit --recursive "${targets[@]}"
+
+# Python code formatter (https://black.readthedocs.io)
 black --check --diff "${targets[@]}"
+
+# Style guide enforcement (https://flake8.pycqa.org)
 flake8 --config .flake8 "${targets[@]}"
+
+# Sort imports (https://pycqa.github.io/isort)
 isort --settings-path .isort.cfg --check --diff "${targets[@]}"
-# mypy: split commands due to https://github.com/python/mypy/issues/4008
+
+# Static type checker (https://mypy.readthedocs.io)
+# Split commands due to https://github.com/python/mypy/issues/4008
 mypy --config-file .mypy.ini "${targets[@]:0:1}"
 mypy --config-file .mypy.ini "${targets[@]:1}"
+
+# Check for errors, enforce a coding standard, look for code smells (http://pylint.pycqa.org)
 pylint --rcfile .pylintrc "${targets[@]}"
-safety check -r api/runtime/requirements.txt -r requirements.txt -r requirements-dev.txt
-# Report code complexity
+
+# Check dependencies for security issues (https://pyup.io/safety)
+# Ignore coverage 5.5 (41002) report, no issues per Snyk: https://snyk.io/vuln/pip:coverage@5.5
+safety check \
+  -i 41002 \
+  -r api/runtime/requirements.txt \
+  -r requirements.txt \
+  -r requirements-dev.txt
+
+# Report code complexity (https://radon.readthedocs.io)
 radon mi "${targets[@]}"
-# Exit with non-zero status if code complexity exceeds thresholds
+
+# Exit with non-zero status if code complexity exceeds thresholds (https://xenon.readthedocs.io)
 xenon --max-absolute A --max-modules A --max-average A "${targets[@]}"
