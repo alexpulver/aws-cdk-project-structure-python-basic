a619ecf449c8ebd5e5dd1dde366954e33c631d2f
Alex Pulver <alex.pulver@gmail.com>
2021-08-10 09:06:23 +0300

    Refactor run-tests.sh

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -11,7 +11,7 @@ flake8 --config .flake8 "${targets[@]}"
 isort --settings-path .isort.cfg --check --diff "${targets[@]}"
 # mypy: split commands due to https://github.com/python/mypy/issues/4008
 mypy --config-file .mypy.ini "${targets[@]:0:1}"
-mypy --config-file .mypy.ini "${targets[@]:1:3}"
+mypy --config-file .mypy.ini "${targets[@]:1}"
 pylint --rcfile .pylintrc "${targets[@]}"
 safety check -r api/runtime/requirements.txt -r requirements.txt -r requirements-dev.txt
 # Report code complexity
