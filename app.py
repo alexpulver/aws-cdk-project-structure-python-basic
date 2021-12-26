2397b9bc8762b294c26a85913007b139fb656a92
Alex Pulver <alex.pulver@gmail.com>
2021-12-26 20:48:34 +0200

    Update constants variable names

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from deployment import UuidGeneratorBackend
 app = cdk.App()
 
 # Development
-UuidGeneratorBackend(app, f"{constants.CDK_APP_NAME}-Dev", env=constants.DEV_ENV)
+UuidGeneratorBackend(app, f"{constants.APP_NAME}-Dev", env=constants.DEV_ENV)
 
 # Production
-UuidGeneratorBackend(app, f"{constants.CDK_APP_NAME}-Prod", env=constants.PROD_ENV)
+UuidGeneratorBackend(app, f"{constants.APP_NAME}-Prod", env=constants.PROD_ENV)
 
 app.synth()
--- constants.py
+++ constants.py
@@ -2,10 +2,8 @@ import os
 
 import aws_cdk as cdk
 
-CDK_APP_NAME = "UuidGeneratorBackend"
-
+APP_NAME = "UuidGeneratorBackend"
 DEV_ENV = cdk.Environment(
     account=os.environ["CDK_DEFAULT_ACCOUNT"], region=os.environ["CDK_DEFAULT_REGION"]
 )
-
 PROD_ENV = cdk.Environment(account="111111111111", region="eu-west-1")
